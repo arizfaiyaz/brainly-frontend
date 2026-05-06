e17d2ac852c6855dfc01ee8446a0ddbee2a2bf72
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-05-06 20:37:45 +0530

    refactor: remove unused Button, PlusIcon, and ShareIcon components; clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css'
-import { Button } from './components/ui/Button'
-import { PlusIcon } from './icons/PlusIcon'
-import { ShareIcon } from './icons/ShareIcon'
+
 
 
 function App() {
   return (
     <>
-      <Button startIcon={<ShareIcon />} size="sm" variant="primary" text="Share"/>
-      <Button startIcon={<PlusIcon />} size="md" variant="secondary" text="Add Content"/>
-      <Button startIcon={<PlusIcon />} size="lg" variant="secondary" text="Add Content"/>
+      
     </>
   )
 }
